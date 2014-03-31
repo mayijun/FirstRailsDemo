c178ef106807452a420a82230436d331b203a57d
ma <ma@work.mayijun.name>
2014-03-31 16:41:32 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aeeiko::Application.config.secret_key_base = '11274e4e58de5c69f29b44ecedfc7f9a1ac6f9ef13929d9a8ff69570a3f9239032a97396597f80bbd7365e650eafd248e24ed7ed7ee6e1aff26a5e0147a902f0'
