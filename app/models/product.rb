42667e2f2ccd25c604172523b113d6de1b994d1c
mayijun <ma.yijun.0@gmail.com>
2014-04-07 15:12:05 +0800

    chr change

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,6 @@ class Product < ActiveRecord::Base
   validates :price,numericality: {greater_than_or_equal_to: 0.01}
   validates :image_url,allow_blank: true,format: {
       with:  %r{\.(gif|jpg|png)\Z}i,
-      message: ‘只接受GIF，JPG或PNG图像’
+      message: '只接受GIF，JPG或PNG图像'
   }
 end
