90899d469bfd540abf6d576cb333ae63cfcd07a4
mayijun <ma.yijun.0@gmail.com>
2014-04-07 16:40:31 +0800

    working

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,8 @@
 class News < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image_url,allow_blank: true,format: {
+      with:  %r{\.(gif|jpg|png)\Z}i,
+      message: '只接受GIF，JPG或PNG图像'
+  }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url,:type, presence: true
   validates :title, uniqueness: true
-  validates :price,numericality: {greater_than_or_equal_to: 0.01}
+  validates :type, :inclusion => { :in => ["票夹/银包", "手包", "背包","公文包","拉杆箱","皮带/配饰"] }
   validates :image_url,allow_blank: true,format: {
       with:  %r{\.(gif|jpg|png)\Z}i,
       message: '只接受GIF，JPG或PNG图像'
