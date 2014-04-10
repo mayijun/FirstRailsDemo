b14a3829bf244e007f018edc889dde38c6e0abe8
mayijun <ma.yijun.0@gmail.com>
2014-04-10 19:57:48 +0800

    rm product related file

--- app/models/product.rb
+++
@@ -1,9 +0,0 @@
-class Product < ActiveRecord::Base
-  validates :title, :description, :image_url,:type, presence: true
-  validates :title, uniqueness: true
-  validates :type, :inclusion => { :in => ["票夹/银包", "手包", "背包","公文包","拉杆箱","皮带/配饰"] }
-  validates :image_url,allow_blank: true,format: {
-      with:  %r{\.(gif|jpg|png)\Z}i,
-      message: '只接受GIF，JPG或PNG图像'
-  }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Aeeiko::Application.routes.draw do
 
   resources :news
 
-  resources :products
-
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
